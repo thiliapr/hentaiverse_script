6cb5297b3cdc8bc40c7983c45791f754c292256a
thiliapr <140580293+thiliapr@users.noreply.github.com>
2026-03-07 23:00:06 +0800

    在遇到小马谜题时添加注释，说明不选的惩罚较小

--- task_bot.py
+++ task_bot.py
@@ -98,6 +98,8 @@ def battle_with_skip_riddle(*args, **kwargs):
         except TokenNotFoundError as e:
             if "function check_submit_button() {" in e.page:
                 # 让谜题自动过期。如果还没过期，那就再等这么多秒
+                # 我知道不选也算是错误，但是 Wiki 说不选的惩罚比乱选的惩罚小，所以就拖吧
+                # 请见 Wiki: https://ehwiki.org/wiki/RiddleMaster
                 print("遇到小马谜题了！")
                 time.sleep(20)
                 continue
