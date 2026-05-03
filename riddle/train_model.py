5b6af676539c15a2d61e99233f2a5c4b8e1dd9a2
thiliapr <140580293+thiliapr@users.noreply.github.com>
2026-05-03 08:31:26 +0800

    参数化训练

--- riddle/train_model.py
+++ riddle/train_model.py
@@ -1,17 +1,29 @@
-from ultralytics import YOLO
+if __name__ == "__main__":
+    # 解析命令行参数
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=8, help="训练的轮数，默认为 %(default)s")
+    parser.add_argument("--batch-size", type=int, default=16, help="每个批次的样本数量，默认为 %(default)s")
+    parser.add_argument("--device", type=int, action="append", default=[], help="使用的 CUDA 设备编号，不指定则代表使用 CPU")
+    parser.add_argument("--workers", type=int, default=8, help="数据加载的工作线程数量，默认为 %(default)s")
+    parser.add_argument("--patience", type=int, default=20, help="早停的耐心值，默认为 %(default)s")
+    args = parser.parse_args()
 
-model = YOLO()
-
-# 训练
-model.train(
-    data="dataset.yaml",
-    epochs=8,
-    imgsz=640,
-    batch=16,
-    device="cpu",
-    workers=8,
-    patience=20,
-    seed=19890604,
-    project="ckpt",
-    exist_ok=True,
-)
+    # 训练
+    from generate_dataset import RIDDLE_IMAGE_SIZE
+    from ultralytics import YOLO
+    model = YOLO()
+    model.train(
+        data="dataset.yaml",
+        rect=True,
+        epochs=args.epochs,
+        imgsz=RIDDLE_IMAGE_SIZE[0],
+        batch=args.batch_size,
+        device="cpu" if args.device else args.device,
+        workers=args.workers,
+        patience=args.patience,
+        seed=19890604,
+        project="./runs",
+        name="ckpt",
+        exist_ok=True,
+    )
