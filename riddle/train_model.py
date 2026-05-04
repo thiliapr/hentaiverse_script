3f7da430bcd3270359560875b790adc36caacd47
thiliapr <140580293+thiliapr@users.noreply.github.com>
2026-05-04 21:32:33 +0800

    copyright for agpl3+

--- riddle/train_model.py
+++ riddle/train_model.py
@@ -1,3 +1,8 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# SPDX-FileCopyrightText: 2026 thiliapr <thiliapr@tutanota.com>
+# SPDX-Package: hentaiverse_script
+# SPDX-PackageHomePage: https://github.com/thiliapr/hentaiverse_script
+
 import argparse
 
 
